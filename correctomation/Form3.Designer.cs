cf60eb90a9b8f628355b7b209a5600342209a065
C0DiNg4FuN <oaj93oaj93@gmail.com>
2017-11-09 12:57:29 +0200

    update V2

---
+++ correctomation/Form3.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace correctomation
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.button_cpps_dir = new System.Windows.Forms.Button();
+            this.button_in_out = new System.Windows.Forms.Button();
+            this.textBox_cpps_dir = new System.Windows.Forms.TextBox();
+            this.textBox_in_out = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_cpp_file_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_properties = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_cpps_dir
+            // 
+            this.button_cpps_dir.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button_cpps_dir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_cpps_dir.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_cpps_dir.ForeColor = System.Drawing.Color.White;
+            this.button_cpps_dir.Location = new System.Drawing.Point(12, 12);
+            this.button_cpps_dir.Name = "button_cpps_dir";
+            this.button_cpps_dir.Size = new System.Drawing.Size(154, 22);
+            this.button_cpps_dir.TabIndex = 1;
+            this.button_cpps_dir.Text = "CPPs directory";
+            this.button_cpps_dir.UseVisualStyleBackColor = false;
+            this.button_cpps_dir.Click += new System.EventHandler(this.button_cpps_dir_Click);
+            // 
+            // button_in_out
+            // 
+            this.button_in_out.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button_in_out.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_in_out.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_in_out.ForeColor = System.Drawing.Color.White;
+            this.button_in_out.Location = new System.Drawing.Point(12, 40);
+            this.button_in_out.Name = "button_in_out";
+            this.button_in_out.Size = new System.Drawing.Size(154, 22);
+            this.button_in_out.TabIndex = 7;
+            this.button_in_out.Text = "input/output directory";
+            this.button_in_out.UseVisualStyleBackColor = false;
+            this.button_in_out.Click += new System.EventHandler(this.button_in_out_Click);
+            // 
+            // textBox_cpps_dir
+            // 
+            this.textBox_cpps_dir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_cpps_dir.Location = new System.Drawing.Point(172, 14);
+            this.textBox_cpps_dir.Name = "textBox_cpps_dir";
+            this.textBox_cpps_dir.Size = new System.Drawing.Size(493, 20);
+            this.textBox_cpps_dir.TabIndex = 8;
+            // 
+            // textBox_in_out
+            // 
+            this.textBox_in_out.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_in_out.Location = new System.Drawing.Point(172, 42);
+            this.textBox_in_out.Name = "textBox_in_out";
+            this.textBox_in_out.Size = new System.Drawing.Size(493, 20);
+            this.textBox_in_out.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(12, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "CPP file name";
+            // 
+            // textBox_cpp_file_name
+            // 
+            this.textBox_cpp_file_name.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_cpp_file_name.Location = new System.Drawing.Point(172, 69);
+            this.textBox_cpp_file_name.Name = "textBox_cpp_file_name";
+            this.textBox_cpp_file_name.Size = new System.Drawing.Size(493, 20);
+            this.textBox_cpp_file_name.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Properties";
+            // 
+            // textBox_properties
+            // 
+            this.textBox_properties.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_properties.Location = new System.Drawing.Point(172, 95);
+            this.textBox_properties.Name = "textBox_properties";
+            this.textBox_properties.Size = new System.Drawing.Size(493, 20);
+            this.textBox_properties.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(653, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 388);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_properties);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_cpp_file_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_in_out);
+            this.Controls.Add(this.textBox_cpps_dir);
+            this.Controls.Add(this.button_in_out);
+            this.Controls.Add(this.button_cpps_dir);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Resize += new System.EventHandler(this.Form3_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_cpps_dir;
+        private System.Windows.Forms.Button button_in_out;
+        private System.Windows.Forms.TextBox textBox_cpps_dir;
+        private System.Windows.Forms.TextBox textBox_in_out;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_cpp_file_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_properties;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
---
+++ correctomation/Form3.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace correctomation
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        //set background gradient
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            if (this.ClientRectangle.Width != 0 && this.ClientRectangle.Height != 0)
+            {
+                LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.White, System.Drawing.SystemColors.GradientActiveCaption, 90F);
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void Form3_Resize(object sender, EventArgs e)
+        {
+            Console.WriteLine(" >>> ResizeEnd");
+            this.Invalidate();
+        }
+
+        private void button_cpps_dir_Click(object sender, EventArgs e)
+        {
+            string cppsDirectory = directoryDialog();
+            if (!string.IsNullOrEmpty(cppsDirectory))
+            {
+                textBox_cpps_dir.Text = cppsDirectory;
+            }
+            //TODO: handle else
+        }
+
+        private void button_in_out_Click(object sender, EventArgs e)
+        {
+            string inOutDirectory = directoryDialog();
+            if (!string.IsNullOrEmpty(inOutDirectory))
+            {
+                textBox_in_out.Text = inOutDirectory;
+            }
+            //TODO: handle else
+        }
+
+        private string directoryDialog()
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                return dialog.SelectedPath;
+            }
+            return string.Empty;
+        }
+
+        private void start(string path, string cppFile)
+        {
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                //ArrayList txtFiles = getSpecificFilesRecursively(dir, "*.txt"); //should be one txt file
+                //ArrayList cppFiles = getSpecificFilesRecursively(dir, "*.cpp");
+
+                //get student number, which is the last chunk of the path
+                string[] n = dir.Split(Path.DirectorySeparatorChar);
+                string studentNumber = n[n.Length - 1];
+
+                //get student name: the txt file name (assuming there is one txt file only)
+                //clean txt file name: remove extention and replace underscore by space
+                string[] s = getSpecificFileRecursively(dir, "*.txt").Split(Path.DirectorySeparatorChar);
+                string studentName = s[s.Length - 1].Replace(".txt", "").Replace("_", " ");
+
+                //get the path of the cpp file to be compiled
+                string cppFilePath = getSpecificFileRecursively(dir, cppFile);
+                Console.WriteLine(studentNumber + " >> " + studentName + " >> " + cppFilePath + " <<");
+            }
+        }
+
+        //returns a list of all the files in a particular directory recursilvely
+        private ArrayList listFilesRecursively(string path)
+        {
+            ArrayList ls = new ArrayList();
+            foreach (string f in Directory.GetFiles(path)) ls.Add(f);
+            foreach (string dir in Directory.GetDirectories(path)) ls.AddRange(listFilesRecursively(dir));
+            return ls;
+        }
+
+        //returns a list of all txt files in a particular directory recursilvely
+        private ArrayList getSpecificFilesRecursively(string path, string pattern)
+        {
+            ArrayList ls = new ArrayList();
+            foreach (string f in Directory.GetFiles(path,pattern)) ls.Add(f);
+            foreach (string dir in Directory.GetDirectories(path)) ls.AddRange(getSpecificFilesRecursively(dir, pattern));
+            return ls;
+        }
+
+        //returns a specific file (txt or cpp) in a particular directory recursilvely
+        private string getSpecificFileRecursively(string path, string pattern)
+        {
+            ArrayList ls = new ArrayList();
+            foreach (string f in Directory.GetFiles(path, pattern)) ls.Add(f);
+            foreach (string dir in Directory.GetDirectories(path)) ls.AddRange(getSpecificFilesRecursively(dir, pattern));
+            if (ls.Count > 0) return ls[0].ToString();
+            else return string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start(textBox_cpps_dir.Text, "p1a.cpp");
+        }
+
+    }
+}
